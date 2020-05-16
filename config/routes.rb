c1bead2177c1a225952b079f72c1ae8ecc12bcbe
Yessica Delgado <yessica@cooltiva.mx>
2020-05-16 16:35:22 -0500

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   resources :users
 
   resources :activities, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resources :reviews, only: [ :new, :create ]
+    resources :categories,
     resources :bookings, only: [ :new, :create ]
   end
   resources :pages, only: [:home]
   resources :reviews, only: [ :destroy ]
-  resources :bookings, only: [ :index]
+  resources :bookings, only: [ :show] do
+    resources :reviews, only: [ :new, :create ]
+    end
+  end
 end
