e3dda4f20d56eb98af2af692fa52a1d805b93e88
Lilia Jerbi <liliajer@buffalo.edu>
2020-05-19 18:26:54 -0500

    geocoder

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,6 @@ class Activity < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :bookings
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
