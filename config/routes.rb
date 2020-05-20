3fc831751b2edc8c26cb75fdc917cf507b84814b
Yessica Delgado <yessica@cooltiva.mx>
2020-05-19 19:59:04 -0500

    update the routes and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  resources :users
 
-  resources :activities, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resources :categories,
-    resources :bookings, only: [ :new, :create ]
+  devise_for :users
+
+  resources :users
+    resources :activities  do
+      resources :bookings, only: [ :new, :create ]
+      resources :reviews, only: [ :new, :create ]
   end
   resources :pages, only: [:home]
-  resources :reviews, only: [ :destroy ]
   resources :bookings, only: [ :show] do
     resources :reviews, only: [ :new, :create ]
-    end
   end
 end
+
