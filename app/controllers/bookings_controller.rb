290f0926a54e00e947217df7a69507205bf6627a
Lilia Jerbi <liliajer@buffalo.edu>
2020-05-28 20:35:44 -0500

    booking front-end

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -34,6 +34,6 @@ class BookingsController < ApplicationController
 private
 
  def booking_params
-  params.require(:booking).permit(  :start_date, :end_date)
+  params.require(:booking).permit(:start_date, :end_date)
  end
 end
