5fe96ea463385a0f4b7977ab930208327bd9b6bd
Yessica Delgado <yessica@cooltiva.mx>
2020-06-11 20:38:32 -0500

    checklist updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :photo, :persona_description])
   end
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
